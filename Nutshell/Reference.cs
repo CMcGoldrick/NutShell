584d76aa09f98245fd8e638a7dc52c7da91d782d
Ciaran McGoldrick <cmcgoldrick@awesemo.com>
2021-06-18 11:58:58 -0500

    initial commit

---
+++ Nutshell/Reference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nutshell
+{
+    class Reference
+    {
+        public void Show()
+        {
+            /*** VERBATIM IDENTIFIER ***/             
+            //you want to use a keyword as an identifier (variable name)
+            string[] @for = { "John", "James", "Joan", "Jamie" };
+            for (int ctr = 0; ctr<@for.Length; ctr++)
+            {
+               Console.WriteLine($"Here is your gift, {@for[ctr]}!");
+            }
+
+            //you want to define a verbatim string literal
+            //e.g. escape characters will not be treated as escape characters but as literal parts of the string
+            string filename1 = @"c:\documents\files\u0066.txt";
+            string filename2 = "c:\\documents\\files\\u0066.txt";
+
+
+
+        }
+}
+}
